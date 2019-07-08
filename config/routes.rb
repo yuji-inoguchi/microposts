f20adf6882289c192644197e8009b279ef1d651e
yuji.inoguchi <y8m1.wa1017@gmail.com>
2019-07-08 15:05:45 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
+  resources :microposts, only: [:create, :destroy]
+  
 end
